b234ca226bf58468e761c3a86239722962580ec9
Gilad Weinberger <giladwe21@gmail.com>
2023-11-28 18:46:33 +0200

    remove to work

--- dev_zone/settings.py
+++ dev_zone/settings.py
@@ -38,8 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'base',
-    'abc'
+    'base'
 ]
 
 MIDDLEWARE = [
@@ -74,7 +73,6 @@ WSGI_APPLICATION = 'dev_zone.wsgi.application'
 
 
 # Database
-#abc
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
