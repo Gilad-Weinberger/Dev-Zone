51ccdaa98c2f18811cefa2e3425596139a367019
Gilad Weinberger <giladwe21@gmail.com>
2023-11-28 22:20:44 +0200

    update menu generate func

--- base/views.py
+++ base/views.py
@@ -3,15 +3,12 @@ from django.shortcuts import render
 def generate_menu(n):
     menu = [
         {"name": "Home", "icon": "fa-regular fa-house", "now": False},
-        {"name": "Home", "icon": "fa-regular fa-house", "now": False},
-        {"name": "Home", "icon": "fa-regular fa-house", "now": False},
-        {"name": "Home", "icon": "fa-regular fa-house", "now": False},
-        {"name": "Home", "icon": "fa-regular fa-house", "now": False},
-        {"name": "Home", "icon": "fa-regular fa-house", "now": False},
-        {"name": "Home", "icon": "fa-regular fa-house", "now": False},
-        {"name": "Home", "icon": "fa-regular fa-house", "now": False},
-        {"name": "Home", "icon": "fa-regular fa-house", "now": False},
-        {"name": "Home", "icon": "fa-regular fa-house", "now": False},
+        {"name": "Projects", "icon": "fa-regular fa-house", "now": False},
+        {"name": "Statistics", "icon": "fa-regular fa-house", "now": False},
+        {"name": "Competitions", "icon": "fa-regular fa-house", "now": False},
+        {"name": "FAQ", "icon": "fa-regular fa-house", "now": False},
+        {"name": "Blog", "icon": "fa-regular fa-house", "now": False},
+        {"name": "Club", "icon": "fa-regular fa-house", "now": False},
     ]
     if n >= len(menu):
         return
@@ -19,4 +16,10 @@ def generate_menu(n):
     return menu
 
 def home(request):
-    return render(request, 'base/home.html')
+    menu = generate_menu(0)
+
+    context = {
+        "menu": menu,
+    }
+
+    return render(request, 'base/home.html', context)
