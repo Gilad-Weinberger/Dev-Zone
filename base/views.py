15cc5b0001a0bb692579aa616e465ca64517380e
Gilad Weinberger <giladwe21@gmail.com>
2023-11-28 22:08:01 +0200

    start html

--- base/views.py
+++ base/views.py
@@ -1,4 +1,22 @@
 from django.shortcuts import render
 
+def generate_menu(n):
+    menu = [
+        {"name": "Home", "icon": "fa-regular fa-house", "now": False},
+        {"name": "Home", "icon": "fa-regular fa-house", "now": False},
+        {"name": "Home", "icon": "fa-regular fa-house", "now": False},
+        {"name": "Home", "icon": "fa-regular fa-house", "now": False},
+        {"name": "Home", "icon": "fa-regular fa-house", "now": False},
+        {"name": "Home", "icon": "fa-regular fa-house", "now": False},
+        {"name": "Home", "icon": "fa-regular fa-house", "now": False},
+        {"name": "Home", "icon": "fa-regular fa-house", "now": False},
+        {"name": "Home", "icon": "fa-regular fa-house", "now": False},
+        {"name": "Home", "icon": "fa-regular fa-house", "now": False},
+    ]
+    if n >= len(menu):
+        return
+    menu[n]["now"] = True
+    return menu
+
 def home(request):
     return render(request, 'base/home.html')
